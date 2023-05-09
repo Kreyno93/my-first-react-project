86befbfd137dec6b42bf3576ef11df0dab3b9f6c
Kreyno93 <michael.wagener@neuefische.de>
2023-05-09 12:48:16 +0200

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    type studentType = {
+        name: string,
+        id: number,
+        laptop?: boolean,
+        lastName: string
+    }
+
+    const student1:studentType = {
+        name: "hallo",
+        id: 2,
+        lastName:"currywurst",
+    };
+
+    const student2:studentType = {
+        name: "hallo",
+        id: 2,
+        laptop:true,
+        lastName:"currywurst"
+    };
+
+    console.log(student1.name);
+
+    function printNames(name1: string, name2: string): string {
+        return "hallo" + name1 + "und" + name2;
+    }
+
+    const arrayNumbers: number[] = [1, 2, 3, 3, 5, 1, 2, 3, 4, 5]
+
+    const foundNumber = arrayNumbers.find((currentNumber: number) => {
+        return currentNumber === 5;
+    })
+
+    // console.log(foundNumber);
+    //
+    //
+    // console.log(printNames("Christian", "Christian"));
+
+
+    return (
+        <div className="App">
+            <header className="App-header">
+            </header>
+        </div>
+    );
 }
 
 export default App;
